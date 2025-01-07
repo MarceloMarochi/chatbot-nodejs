4b482241421476b5c14cd9dd094fe7113b90715a
Marcelo Gabriel Marochi <marce.marochi@gmail.com>
2025-01-07 11:34:24 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const ejs = require('ejs');
+
+/* Variable que usa la function express */
+const app = express();
+
+/* Le decimos que use el directorio public */
+app.use(express.static('public'));
+
+/* Configuración motor de vistas ejs */
+app.set('view engine', 'ejs');
+
+/* Especifico el puerto de conexión */
+app.listen(8080, () => {
+    console.log('Servidor iniciado');
+});
+
+app.get('/', (req, res) => {
+    res.render('pages/index');
+});
---
+++ readme.md
@@ -0,0 +1,10 @@
+# First time create
+
+1. npm install 
+2. npm init
+3. npm install --save express
+4. npm install --save request
+5. npm install ejs
+
+# Execute
+- node index.js
